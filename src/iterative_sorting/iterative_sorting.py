658e137773a154e61af8722bdb83d0beb1df65db
kly001 <karenly1961@gmail.com>
2020-06-03 22:02:58 -0400

    Working on problems in iterative_sorting.py

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -15,6 +15,63 @@ def selection_sort(arr):
     return arr
 
 
+############################################
+# ## Bubble Sort ==>
+# ## Possible Solutions:
+
+# ##  1.  https://www.geeksforgeeks.org/python-program-for-selection-sort/:
+# ## # Traverse through all array elements 
+# for i in range(len(A)): 
+      
+# ##    # Find the minimum element in remaining  
+# ##     # unsorted array 
+#     min_idx = i 
+#     for j in range(i+1, len(A)): 
+#         if A[min_idx] > A[j]: 
+#             min_idx = j 
+              
+# ##     # Swap the found minimum element with  
+# ##     # the first element         
+#     A[i], A[min_idx] = A[min_idx], A[i] 
+
+####################################################
+
+
+# ## 2. https://www.educba.com/bubble-sort-in-python/ 
+
+# def bubble_Sort(arr):
+# m = len(arr)
+# # Traverse through all the array elements
+# for u in range(m):
+# for v in range(0, m-u-1):
+# # traverse the array from 0 to m-u-1
+# # Swap if the element is greater than adjacent next one
+# if arr[v] > arr[v+1] :
+# arr[v], arr[v+1] = arr[v+1], arr[v]
+
+######################################################################
+
+# ## 3. https://www.programminginpython.com/bubble-sort-algorithm-python/
+
+	# def bubble_sort(sort_list):
+	#     for j in range(len(sort_list)):
+	#         for k in range(len(sort_list) - 1):
+	#             if sort_list[k] > sort_list[k + 1]:
+	#                 sort_list[k], sort_list[k + 1] = sort_list[k + 1], sort_list[k]
+	#     print(sort_list)
+	
+	
+	# lst = []
+	# size = int(input("Enter size of the list: \t"))
+	
+	# for i in range(size):
+	#     elements = int(input("Enter the element: \t"))
+	#     lst.append(elements)
+	
+	# bubble_sort(lst)
+
+#################################################################
+
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
